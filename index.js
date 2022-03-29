81babe4f8f23bc65f3d418bb8938b73ed202bedc
Nziie3 <nziie124@gmail.com>
2022-03-29 12:20:22 -0700

    added notifications

---
+++ index.js
@@ -0,0 +1,16 @@
+let x;
+let toast = document.getElementById("toast");
+function showToast(){
+    clearTimeout(x);
+    toast.style.transform = "translateX(0)";
+    x = setTimeout(()=>{
+        toast.style.transform = "translateX(400px)"
+    }, 4000);
+}
+function closeToast(){
+    toast.style.transform = "translateX(400px)";
+}
+
+window.addEventListener('load', (event) => {
+  showToast();
+});
