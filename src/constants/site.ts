ff5391e8862bda7929c995c3cf9a082f5a097d43
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-07-27 13:38:43 -0700

    Fix invite.mdx

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,5 +1,5 @@
 export const site: Record<string, any> = {
-    'name': 'Server Manager Docs',
+    'name': 'Server Manager',
     'tagline': 'Server Manager Discord bot - let the bot do the hard work for you.',
     'url': 'https://servermanagerbot.ml',
     'orgName': 'Server Manager Development Team'
