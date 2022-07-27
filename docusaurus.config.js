7d9d1246f6a419914d86bf20c5486fa660d9c572
Server Manager Dev Team <102108169+Server-Manager-Dev-Team@users.noreply.github.com>
2022-07-27 07:33:58 -0700

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,78 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/vsLight");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Server Manager Docs",
+  tagline:
+    "Imagine a simple customizable multipurpose bot",
+  url: "https://servermanagerbot.ml",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  trailingSlash: false,
+  deploymentBranch:"main",
+  organizationName: "Server-Manager-Dev-Team", // Usually your GitHub org/user name.
+  projectName: "Server-Manager-Bot.github.io", // Usually your repo name.
+  plugins: ["docusaurus-plugin-sass"],
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          editUrl:
+            "https://github.com/Server-Manager-Dev-Team/Server-Manager-Bot.github.io/tree/main",
+          routeBasePath: "/",
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+        },
+        theme: {
+          customCss: require.resolve("./src/scss/main.scss"),
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    docs: {
+      sidebar: {
+        autoCollapseCategories: true,
+      },
+    },
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,  // TODO: Support light mode (I know, I know. Eww. I'm sorry.)
+    },
+    navbar: {
+      title: "Server Manager",
+      hideOnScroll: true,
+      items: [
+        // TODO: Post v2.0: Version the docs
+        // {
+        //   type: 'docsVersionDropdown',
+        //   position: 'right',
+        // },
+        // TODO: Locales
+        // {
+        //   type: 'localeDropdown',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Server Manager Development, All rights reserved.`,
+    },
+    prism: {
+      defaultLanguage: "python",
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+};
+
+module.exports = config;
